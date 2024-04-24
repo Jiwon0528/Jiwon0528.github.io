35e92aa10e6f848bc3c7cb9824012b304829e9fb
Jiwon0528 <144713083+Jiwon0528@users.noreply.github.com>
2024-04-24 17:00:10 +0000

    update main.js scripts

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+
+for (i = 1; i < 11; i++) {
+    println(i * i);
+}
+for (i = 5; i < 5; i++) {
+    println (i-1)
+    print("Blastoff!")
+}
+for (i=0; i < 50; i++) {
+    println (i + 2)
+}
+let sum = 0;
+for (i=0; i<100; i++) {
+    println(i + sum)
+}
+let iFactorial = i;
+for (i=1; i<11; i++) {
+    
+}
+
