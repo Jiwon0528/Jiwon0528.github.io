7c0ca1b17004b0ec37f6107ed7c187054dbfc5d1
Jiwon0528 <144713083+Jiwon0528@users.noreply.github.com>
2024-04-26 15:47:45 +0000

    update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,17 +2,21 @@ console.log("Task 1");
 for (let i = 1; i < 11; i++) {
     console.log(i * i);
 }
+console.log("Task 2")
 for (let i = 5; i < 5; i++) {
     console.log (i - 1)
     print("Blastoff!")
 }
+console.log("Task 3")
 for (let i = 2; i < 26; i++) {
     console.log (i + 2)
 }
+console.log("Task 4")
 let sum = 0;
 for (let i = 0; i < 100; i++) {
     console.log (i + sum)
 }
+console.lot("Task 5")
 let iFactorial = i;
 for (let i = 1; i < 11; i++) {
     console.log (i * iFactorial)
