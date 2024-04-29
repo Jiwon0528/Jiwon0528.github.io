4982da91e5e52296a72c1db37a2a4cc82656e741
Jiwon0528 <144713083+Jiwon0528@users.noreply.github.com>
2024-04-29 16:11:13 +0000

    update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,41 +3,54 @@ for (let i = 1; i < 11; i++) {
     console.log(i * i);
 }
 console.log("Task 2")
-for (let i = 5; 0 < i < 6; i--) {
+for (let i = 5; 0 < i ; i--) {
     console.log (i)
     if (i == 1){
         console.log("Blastoff!");
     }
 }
 console.log("Task 3")
-for (let i = 2; i < 100; i++) {
+for (let i = 2; i < 51; i = i + 2) {
     console.log (i)
 }
 console.log("Task 4")
 let sum = 0;
-for (let i = 0; i < 100; i++) {
-    console.log (i + sum)
+for (let i = 0; i < 101; i++) {
+    sum=i+sum;
 }
+console.log(sum);
+
 console.lot("Task 5")
 let iFactorial = i;
 for (let i = 1; i < 11; i++) {
-    console.log (i * iFactorial)
+    iFactorial = i * iFactorial;
 }
-console.lot("Task 6")
-let arr = [3,1,4,1,5,9]
-    console.log(arr.length)
+console.log(iFactorial)
 
-console.lot("Task 7")
-let arr = [3,1,4,1,5,9]
+console.log("Task 6")
+let arr = [3,1,4,1,5]
+for (let i=0; i<arr.length; i++) {
+    console.log(arr[0,1,2,3,4])
+}
+    
+console.log("Task 7")
 for (let i = 0; i <arr.length; i++) {
     console.log(arr.at(-1))    
 }
-console.lot("Task 8")
-let arr = [1,2,3,4,5,6,7,8,9,10] {
-    console.log(arr)
+console.log("Task 8")
+let arr2 = [];
+for (let i=0;i<arr3.length; i++) {
+    
+    console.log(arr.push)
 }
 
-console.lot("Task 9")
-let arr = [0,1,1,2,3,5,8,13,21,34]
+console.log("Task 9")
+let arr3 = [i]
+for (let i=0; i<arr3.length; i++) {
+    Fibonacci = arr[i]+arr[i-1];
+    arr3.push(Fibonacci)
 
-console.lot("Task 10")
+}
+    log()
+
+console.log("Task 10")
