4e493af157cae65d2c685d45b43b7921a1161bdc
Jiwon0528 <144713083+Jiwon0528@users.noreply.github.com>
2024-04-29 16:20:08 +0000

    update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -21,36 +21,36 @@ for (let i = 0; i < 101; i++) {
 console.log(sum);
 
 console.log("Task 5")
-let iFactorial = i;
+let iFactorial = 1;
 for (let i = 1; i < 11; i++) {
     iFactorial = i * iFactorial;
+    console.log(iFactorial);
 }
-console.log(iFactorial)
 
 console.log("Task 6")
 let arr = [3,1,4,1,5]
 for (let i=0; i<arr.length; i++) {
-    console.log(arr[0,1,2,3,4])
+    console.log(arr[i])
 }
     
 console.log("Task 7")
 for (let i = 0; i <arr.length; i++) {
-    console.log(arr.at(-1))    
+    console.log(arr.at(-i-1))    
 }
 console.log("Task 8")
 let arr2 = [];
-for (let i=0;i<arr3.length; i++) {
+for (let i=0;i<11; i++) {
     
-    console.log(arr.push)
+    arr2.push(i**3);
 }
-
+console.log(arr2)
 console.log("Task 9")
-let arr3 = [i]
-for (let i=0; i<arr3.length; i++) {
-    Fibonacci = arr[i]+arr[i-1];
+let arr3 = [0,1]
+for (let i=1; i<11; i++) {
+   let Fibonacci = arr[i]+arr[i-1];
     arr3.push(Fibonacci)
 
 }
-    log()
+   console.log(arr3)
 
 console.log("Task 10")
