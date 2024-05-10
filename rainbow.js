f7e730bb2a4c41c96cd372d89d2899dfbff18685
Jiwon0528 <144713083+Jiwon0528@users.noreply.github.com>
2024-05-10 16:22:39 +0000

    update rainbow.js

--- rainbow.js
+++ rainbow.js
@@ -1,5 +1,27 @@
-console.log("Task 1")
-let arr = ["red", "blue", "yellow", "green", "purple", "orange", "black"]
+console.log("Task 1");
+let arr6 = ["red", "blue", "yellow", "green", "purple", "orange", "black", "midnightblue", "cyan", "magenta", "pink"];
+let list = document.createElement('ul'); 
+for (let i=0; i<arr6.length; i++) {
+    console.log("Start loop iteration");
+    console.log("arr6[i]: " + arr6[i]);
 
-console.log("Task 2")
-<ul> arr </ul>
+    let item = document.createElement('li');
+    item.innerText = arr6[i];
+    item.style.color = arr6[i];
+    list.appendChild(item);
+    document.body.appendChild(list);
+    console.log("End loop iteration");
+}        
+
+// for (let i=0; i<arr6.length; i++) {
+//     console.log("Start loop iteration");
+//     console.log("arr6[i]: " + arr6[i]);
+
+//     let item = document.createElement('li');
+//     item.innerText = arr6[i];
+//     item.style.color = arr6[i];
+//     list.appendChild(item);
+
+//     console.log("End loop iteration");
+// }        
+// document.body.appendChild(list);
