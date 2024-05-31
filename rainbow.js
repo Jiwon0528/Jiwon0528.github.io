d60b5d67c8fe6745d05bf365525d39bd63978221
Jiwon0528 <144713083+Jiwon0528@users.noreply.github.com>
2024-05-31 16:36:06 +0000

    update Paris.html

--- rainbow.js
+++ rainbow.js
@@ -13,15 +13,3 @@ for (let i=0; i<arr6.length; i++) {
     console.log("End loop iteration");
 }        
 
-// for (let i=0; i<arr6.length; i++) {
-//     console.log("Start loop iteration");
-//     console.log("arr6[i]: " + arr6[i]);
-
-//     let item = document.createElement('li');
-//     item.innerText = arr6[i];
-//     item.style.color = arr6[i];
-//     list.appendChild(item);
-
-//     console.log("End loop iteration");
-// }        
-// document.body.appendChild(list);
